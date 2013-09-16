7899acc917b8b995599a080dcfe2d31d9f4caedb
Fredrik Degerlund <fredrikdegerlund@yahoo.se>
2013-09-16 14:29:38 +0200

    Ny klass: Lingon

---
+++ Lingon/Lingon/Lingon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lingon
+{
+    class Lingon
+    {
+        int Vikt {get; set;}
+        Lingon(int vikt)
+        {
+            Vikt = vikt;
+        }
+    }
+}
